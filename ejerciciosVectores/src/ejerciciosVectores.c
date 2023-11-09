a63ce2476107dc562327d76197065268c2154806
antonio <antonio@DESKTOP-5RQ335U>
2023-11-09 12:39:31 +0100

    main para vectores
    sin terminar

---
+++ ejerciciosVectores/src/ejerciciosVectores.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : ejerciciosVectores.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "vectores.h"
+
+int main(void) {
+	printf("Sin terminar. Borrrrrrado");
+
+	return EXIT_SUCCESS;
+}
